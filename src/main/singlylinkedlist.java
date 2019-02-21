6654e59d01fc145662f51d085df6802e5b0992bd
rahulubansal <rahulubansal@gmail.com>
2019-02-21 08:31:23 +0530

    added parameter(next) to constructor

--- src/main/singlylinkedlist.java
+++ src/main/singlylinkedlist.java
@@ -1,12 +1,19 @@
 package main;
 
 public class singlylinkedlist {
-}
-private static class node{
-    private int data;
-    private node next;
-    public node(int data){
+    private static class node {
+        private int data;
+        private node next;
+
+        public node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public node(int data, node next) {
+            this.data = data;
+            this.next = next;
+        }
 
     }
-
 }
